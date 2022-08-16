38a98104c5e5a82b8e4e6f475dee4d4e08fa0bf1
TomTruyen <tom.truyen@gmail.com>
2022-08-16 18:48:38 +0200

    feat: exercise actions

--- app/src/main/java/com/tomtruyen/Fitoryx/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/tomtruyen/Fitoryx/ui/exercise/ExerciseFragment.kt
@@ -1,12 +1,15 @@
 package com.tomtruyen.Fitoryx.ui.exercise
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.tomtruyen.Fitoryx.R
 import com.tomtruyen.Fitoryx.helper.BounceEdgeEffectFactory
 import org.koin.android.ext.android.inject
@@ -25,19 +28,43 @@ class ExerciseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setActions()
+        setRecyclerView(view)
+
+        view.findViewById<FloatingActionButton>(R.id.add_exercise_fab).setOnClickListener {
+            Toast.makeText(context, "Add exercise click!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setActions() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.exercise_actions_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when(menuItem.itemId) {
+                    R.id.action_filter -> {
+                        Toast.makeText(context, "Filter exercise click!", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_search -> {
+                        Toast.makeText(context, "Search exercise click!", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun setRecyclerView(view: View) {
         val recyclerView = view.findViewById<RecyclerView>(R.id.exercise_recyler_view)
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             adapter = ExerciseAdapter(viewModel.exercises)
             edgeEffectFactory = BounceEdgeEffectFactory()
         }
-
-//        val anim: SpringAnimation = SpringAnimation(recyclerView, SpringAnimation.TRANSLATION_Y)
-//            .setSpring(
-//                SpringForce()
-//                .setFinalPosition(0f)
-//                .setDampingRatio(SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY)
-//                .setStiffness(SpringForce.STIFFNESS_LOW)
-//            )
     }
 }
