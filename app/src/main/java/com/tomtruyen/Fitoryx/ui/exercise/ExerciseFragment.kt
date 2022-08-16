3d95f5eb063c4af2ba83438208164cf81302e4be
TomTruyen <tom.truyen@gmail.com>
2022-08-16 18:27:58 +0200

    feat: RecyclerView bounce effect

--- app/src/main/java/com/tomtruyen/Fitoryx/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/tomtruyen/Fitoryx/ui/exercise/ExerciseFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tomtruyen.Fitoryx.R
+import com.tomtruyen.Fitoryx.helper.BounceEdgeEffectFactory
 import org.koin.android.ext.android.inject
 
 class ExerciseFragment : Fragment() {
@@ -25,6 +26,18 @@ class ExerciseFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.exercise_recyler_view)
-        recyclerView.adapter = ExerciseAdapter(viewModel.exercises)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = ExerciseAdapter(viewModel.exercises)
+            edgeEffectFactory = BounceEdgeEffectFactory()
+        }
+
+//        val anim: SpringAnimation = SpringAnimation(recyclerView, SpringAnimation.TRANSLATION_Y)
+//            .setSpring(
+//                SpringForce()
+//                .setFinalPosition(0f)
+//                .setDampingRatio(SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY)
+//                .setStiffness(SpringForce.STIFFNESS_LOW)
+//            )
     }
 }
