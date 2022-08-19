9bcf172d1afcabdad3e2c850c42b4a523b746c34
TomTruyen <tom.truyen@gmail.com>
2022-08-19 19:38:48 +0200

    feat: login + register screen design

---
+++ app/src/main/java/com/tomtruyen/Fitoryx/ui/authentication/RegisterFragment.kt
@@ -0,0 +1,35 @@
+package com.tomtruyen.Fitoryx.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
+import com.tomtruyen.Fitoryx.R
+
+class RegisterFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.login_text_view).setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.setCustomAnimations(
+                    R.anim.enter_left_to_right,
+                    R.anim.exit_left_to_right,
+                    R.anim.enter_right_to_left,
+                    R.anim.exit_right_to_left
+                )
+                ?.replace(R.id.fragment_container_view, LoginFragment())
+                ?.commit()
+        }
+    }
+}
