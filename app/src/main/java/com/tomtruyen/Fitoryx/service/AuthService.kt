24c1472a2ffa6a862be43a45036167190b884882
TomTruyen <tom.truyen@gmail.com>
2022-08-23 20:18:01 +0200

    fix: made auth val const

--- app/src/main/java/com/tomtruyen/Fitoryx/service/AuthService.kt
+++ app/src/main/java/com/tomtruyen/Fitoryx/service/AuthService.kt
@@ -1,11 +1,10 @@
 package com.tomtruyen.Fitoryx.service
 
-import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 object AuthService {
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     fun isLoggedIn(): Boolean {
         return auth.currentUser != null
